c85f70f91235cb50b07859d496406579f1b3690b
Jhon Cruz <jhon.cruz@skaphe.com>
2014-03-28 23:15:04 -0500

    User auth

---
+++ server/config.js
@@ -0,0 +1,16 @@
+/**
+ * Created by jhon on 28/03/14.
+ */
+
+var config = {
+    twitter: {
+        key: 'WsAjdTqfDLVWLAzo6n3dVZA0c',
+        secret: 'NikhCMOWezmmD44H9J93AECswA5AhvxsB1k60ZBiIAckjhAHf0'
+    },
+    facebook: {
+        id: '673600549345106',
+        secret: 'e6852f13c2ec9287761c92aa7719fcc7'
+    }
+}
+
+module.exports = config;
