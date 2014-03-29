f5793f6ef2f6aca06fee8d87e1251f83a4b42e75
Jhon Cruz <jhon.cruz@skaphe.com>
2014-03-29 07:26:41 -0500

    Markers test

--- server/public/js/main.js
+++ server/public/js/main.js
@@ -8,6 +8,8 @@ BogoData.options = {
     localStorageItem: 'BgD0'
 };
 
+BogoData.markersArray = [];
+
 BogoData.init = function () {
     BogoData.initMap();
     BogoData.setRoutes();
@@ -20,11 +22,25 @@ BogoData.setRoutes = function () {
         }).done(function (data) {
                 var encrypted = CryptoJS.AES.encrypt(JSON.stringify(data), BogoData.options.passphrase);
                 //var decrypted = CryptoJS.AES.decrypt(encrypted, BogoData.options.passphrase);
-                localStorage.setItem(BogoData.options.localStorageItem, encrypted);
 
-                $("#signInButton").hide();
-                $("#signOutButton").show();
+                if (data) {
+                    $("#signInButton").hide();
+                    $("#signOutButton").show();
+                    localStorage.setItem(BogoData.options.localStorageItem, encrypted);
+                } else {
+                    $("#signInButton").show();
+                    $("#signOutButton").hide();
+                    localStorage.removeItem(BogoData.options.localStorageItem);
+                }
+                //reset URL
+                $.router.go("/", "");
 
+            }).fail(function (jqXHR, textStatus, errorThrown) {
+                $("#signInButton").show();
+                $("#signOutButton").hide();
+                localStorage.removeItem(BogoData.options.localStorageItem);
+                //reset URL
+                $.router.go("/", "");
             });
     });
 }
@@ -45,7 +61,34 @@ BogoData.initMap = function () {
 
     var map = new google.maps.Map(mapCanvas, mapOptions);
 
+    var georssLayer = new google.maps.KmlLayer('http://api.flickr.com/services/feeds/geo/?g=322338@N20&lang=en-us&format=feed-georss');
+    georssLayer.setMap(map);
+
     BogoData.map = map;
+
+    var html = "<table>" +
+        "<tr><td>Name:</td> <td><input type='text' id='name'/> </td> </tr>" +
+        "<tr><td>Address:</td> <td><input type='text' id='address'/></td> </tr>" +
+        "<tr><td>Type:</td> <td><select id='type'>" +
+        "<option value='bar' SELECTED>bar</option>" +
+        "<option value='restaurant'>restaurant</option>" +
+        "</select> </td></tr>" +
+        "<tr><td></td><td><input type='button' value='Save & Close' onclick='saveData()'/></td></tr>";
+
+
+    var infowindow = new google.maps.InfoWindow({
+        content: html
+    });
+
+    google.maps.event.addListener(BogoData.map, "click", function (event) {
+        marker = new google.maps.Marker({
+            position: event.latLng,
+            map: BogoData.map
+        });
+        google.maps.event.addListener(marker, "click", function () {
+            infowindow.open(BogoData.map, marker);
+        });
+    });
 }
 
 BogoData.isLoggedIn = function () {
