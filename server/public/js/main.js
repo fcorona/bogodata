a9060d7901984eaccd7b170914248cd55f720e25
Jhon Cruz <jhon.cruz@skaphe.com>
2014-03-29 05:49:41 -0500

    Google map added. Better support for sessions and social networks

--- server/public/js/main.js
+++ server/public/js/main.js
@@ -1,32 +1,55 @@
-require.config({
-	paths: {
-		angular: '../../bower_components/angular/angular',
-		angularRoute: '../../bower_components/angular-route/angular-route',
-        angularResource: '../../bower_components/angular-resource/angular-resource',
-		text: '../../bower_components/requirejs-text/text'
-	},
-	shim: {
-		'angular' : {'exports' : 'angular'},
-		'angularRoute': ['angular'],
-        'angularResource': ['angular']
-	},
-	priority: [
-		"angular"
-	]
-});
+/**
+ * Created by jhon on 29/03/14.
+ */
+var BogoData = {};
 
-//http://code.angularjs.org/1.2.1/docs/guide/bootstrap#overview_deferred-bootstrap
-window.name = "NG_DEFER_BOOTSTRAP!";
+BogoData.options = {
+    passphrase: 'dg76sdgdfg',
+    localStorageItem: 'BgD0'
+};
 
-require( [
-	'angular',
-	'app',
-	'routes'
-], function(angular, app, routes) {
-	'use strict';
-	var $html = angular.element(document.getElementsByTagName('html')[0]);
+BogoData.init = function () {
+    BogoData.initMap();
+    BogoData.setRoutes();
+}
 
-	angular.element().ready(function() {
-		angular.resumeBootstrap([app['name']]);
-	});
-});
+BogoData.setRoutes = function () {
+    $.router.add('/auth/refresh', function () {
+        $.ajax({
+            url: "/auth/user"
+        }).done(function (data) {
+                var encrypted = CryptoJS.AES.encrypt(JSON.stringify(data), BogoData.options.passphrase);
+                //var decrypted = CryptoJS.AES.decrypt(encrypted, BogoData.options.passphrase);
+                localStorage.setItem(BogoData.options.localStorageItem, encrypted);
+
+                $("#signInButton").hide();
+                $("#signOutButton").show();
+
+            });
+    });
+}
+
+BogoData.showSignIn = function () {
+    $("#signIn").dialog({
+        modal: true
+    });
+}
+
+BogoData.initMap = function () {
+    var mapCanvas = document.getElementById('map_canvas');
+    var mapOptions = {
+        center: new google.maps.LatLng(4.670332, -74.086028),
+        zoom: 12,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+
+    BogoData.map = map;
+}
+
+BogoData.isLoggedIn = function () {
+
+}
+
+google.maps.event.addDomListener(window, 'load', BogoData.init);
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,7 +4,11 @@
 module.exports = function (app, passport) {
     app.get('/logout', function (req, res) {
         req.logout();
-        res.redirect('/');
+        res.redirect("/#!/auth/refresh");
+    });
+
+    app.get('/auth/user', function (req, res) {
+        return res.status(200).json(req.user);
     });
 
     app.get('/auth/twitter', passport.authenticate('twitter'));
@@ -12,10 +16,10 @@ module.exports = function (app, passport) {
     app.get('/auth/facebook', passport.authenticate('facebook'));
 
     app.get('/auth/twitter/callback', passport.authenticate('twitter'), function (req, res) {
-        res.redirect("http://bogodata.org");
+        res.redirect("/#!/auth/refresh");
     });
 
     app.get('/auth/facebook/callback', passport.authenticate('facebook'), function (req, res) {
-        res.redirect("http://bogodata.org");
+        res.redirect("/#!/auth/refresh");
     });
 }
